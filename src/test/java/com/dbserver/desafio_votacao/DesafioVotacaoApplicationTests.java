46ba9316c99e0a7cc471424b6bddf0464a083eae
Hygor Barros <hygordebarross@gmail.com>
2025-01-14 22:43:56 -0300

    Commit inicial do desafio-votacao

---
+++ src/test/java/com/dbserver/desafio_votacao/DesafioVotacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbserver.desafio_votacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioVotacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
